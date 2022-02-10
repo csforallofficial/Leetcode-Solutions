98f2c1e3af09e2ab8fece76f6320b20971cb6bf7
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2022-02-10 22:37:25 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (89.23%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    int dp[101][101];
-    int helper(int i, int j, int m, int n){
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(i>=m || j>=n)
-            return 0;
-        if(dp[i][j])
-            return dp[i][j];
-        else
-        return dp[i][j] = helper(i+1,j,m,n) + helper(i,j+1,m,n);
-    }
     int uniquePaths(int m, int n) {
-        int ans=helper(0,0,m,n);
-        return ans;
+        int N = n + m - 2;
+            int r = m - 1; 
+            double res = 1;
+            
+            for (int i = 1; i <= r; i++)
+                res = res * (N - r + i) / i;
+            return (int)res;
     }
 };
