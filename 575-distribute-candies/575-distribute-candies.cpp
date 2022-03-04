d28c003553c42300c4ebd1c1636e4df0d7ee38e0
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2022-03-04 20:11:08 +0530

    Time: 734 ms (5.01%), Space: 120.2 MB (11.34%) - LeetHub

---
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        set<int> st;
+        for(int i = 0; i < candyType.size(); i++)
+            st.insert(candyType[i]);
+        if(st.size()<candyType.size()/2)
+            return st.size();
+        else
+            return candyType.size()/2;
+    }
+};
